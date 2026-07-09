7d554a4936e3baa0b0d8821b7ff170cc2d6558e4
Alexandra Velasquez <tm901345@continental.edu.pe>
2026-07-09 15:15:34 +0000

    Update app layout and event form design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container">
-        <Link className="navbar-brand fw-bold" to="/">
-          Eventos Universitarios
-        </Link>
+    <>
+      <header className="top-header">
+        <h1>Eventos Universitarios</h1>
+      </header>
 
-        <div className="navbar-nav">
-          <Link className="nav-link" to="/">Inicio</Link>
-          <Link className="nav-link" to="/events">Eventos</Link>
-          <Link className="nav-link" to="/events/new">Nuevo Evento</Link>
-          <Link className="nav-link" to="/participants">Participantes</Link>
-          <Link className="nav-link" to="/enrollments">Inscripciones</Link>
-        </div>
-      </div>
-    </nav>
+      <nav className="side-menu">
+        <Link to="/">Inicio</Link>
+        <Link to="/events">Eventos</Link>
+        <Link to="/events/new">Nuevo Evento</Link>
+        <Link to="/participants">Participantes</Link>
+        <Link to="/enrollments">Inscripciones</Link>
+      </nav>
+    </>
   );
 }
 
